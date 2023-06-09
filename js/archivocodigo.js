c8658ef2156c5b1c430949f9dcd4b9941ecb072c
César Acosta <damianac777@gmail.com>
2023-06-09 20:07:27 -0300

    agregue boton de funcion de js

---
+++ js/archivocodigo.js
@@ -0,0 +1,15 @@
+
+alert("suscribite a nuestra página web");
+
+function DatoU(){
+var Manolo = prompt("introduce tu código promocional: ");
+//alert("Felicidades tu código es " + Manolo); 
+
+if(Manolo = Manolo){
+    alert("Felicidades tu código es valido");
+
+}
+}
+
+
+
